4a874fb561c33c16cd798737e78c184fb3e7f434
George Byte <mr.georgebyte@gmail.com>
2023-06-15 08:50:59 +0200

    Add a placeholder for shared HierarchicalTable component

---
+++ src/shared/components/HierarchicalTable/HierarchicalTable.tsx
@@ -0,0 +1,7 @@
+import type {Component} from "solid-js";
+
+import styles from "./HierarchicalTable.module.css";
+
+export const HierarchicalTable: Component = () => {
+    return <div class={styles.HierarchicalTable}></div>;
+};
