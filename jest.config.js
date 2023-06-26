78b81250c4d11d9b532da359ddf0adde4e865a1b
George Byte <mr.georgebyte@gmail.com>
2023-06-26 08:18:49 +0200

    Install jest to run unit tests

---
+++ jest.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+    preset: "ts-jest",
+    testEnvironment: "jsdom",
+    transform: {
+        "^.+\\.tsx?$": [
+            "ts-jest",
+            {
+                babelConfig: {
+                    presets: ["babel-preset-solid", "@babel/preset-env"],
+                },
+                tsconfig: "tsconfig.json",
+            },
+        ],
+        "\\.module\\.css$": "jest-css-modules-transform",
+    },
+    moduleNameMapper: {
+        "solid-js/web": "<rootDir>/node_modules/solid-js/web/dist/web.cjs",
+        "solid-js": "<rootDir>/node_modules/solid-js/dist/solid.cjs",
+    },
+};
